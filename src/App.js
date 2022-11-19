f8639948bea3635b34302cd02197f05fe6029e4e
vinaymarni <iamvinnu.m@gmail.com>
2022-11-19 11:45:04 +0530

    updated with world map bg

--- src/App.js
+++ src/App.js
@@ -30,11 +30,13 @@ function App() {
 
   }
   return (
-    <div className= {`App ${mode}`} >     
+
+    <div className= {`App ${mode}`} >   
+      <img src="https://res.cloudinary.com/di01osmzz/image/upload/v1668837368/world-map_thhktd.png" alt="" />  
       <div className="switch-outer" style={{justifyContent: turn }}>
         <img style={{display: upArrow, marginBottom: "20px"}} className="arrow" src="https://res.cloudinary.com/di01osmzz/image/upload/v1668765842/fast-forward-double-right-arrows-symbol_3_gijmts.png" alt="" />
         <div className={`switch-inner ${mode}`}  onClick={isSwitch}>
-          <div style={{height: "4px", width: "13px", backgroundColor: dot, borderRadius: "5px"}}></div>
+          <div className={`led-${mode}`} style={{height: "4px", width: "13px", backgroundColor: dot, borderRadius: "5px"}} ></div>
           <h3 style={{color: "white", fontSize: "10px"}}>{name}</h3>
           <div className="on-icon-con" >
             <img src="https://cdn-icons-png.flaticon.com/128/4139/4139573.png" alt="" className="onIcon" />
