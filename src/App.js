90320278be749c4da91690185fdb076e80832989
vinaymarni <iamvinnu.m@gmail.com>
2022-12-01 19:31:42 +0530

    using hooks object

--- src/App.js
+++ src/App.js
@@ -3,47 +3,39 @@ import './App.css';
 
 function App() {
   const[position, setPosition] = useState(false)
-  const[turn, setTurn] = useState("flex-end")
-  const[dot, setDot] = useState("gray")
-  const[name, setName] = useState("STOP")
-  const[upArrow, setUpArrow] = useState("block")
-  const[downArrow, setDownArrow] = useState("none")
-  const[mode, setMode] = useState("light")
-  
+  const[data, setData] = useState({
+    turn : "flex-end",
+    dot : "gray",
+    name : "STOP",
+    upArrow : "block",
+    downArrow : "none",
+    mode : "light"
+  })
+
   const isSwitch = () => {
     setPosition(!position)
-    if(position === true){
-      setTurn("flex-end")
-      setDot("gray")
-      setName("STOP")
-      setUpArrow("block")
-      setDownArrow("none")
-      setMode("light")
-    }else{
-      setTurn("flex-start")
-      setDot("#00FF78")
-      setName("START")
-      setUpArrow("none")
-      setDownArrow("block")
-      setMode("dark")
-    }
-
+      if(position === true){       
+        setData({turn : "flex-end", dot : "gray", name : "STOP", upArrow : "block", downArrow : "none", mode : "light"   })
+      }else{
+        setData({turn : "flex-start", dot : "#00FF78", name : "START", upArrow : "none", downArrow : "block", mode : "dark"   })   
+      }
   }
-  return (
 
-    <div className= {`App ${mode}`} >   
+  return (
+    <div className= {`App ${data.mode}`} >   
       <img src="https://res.cloudinary.com/di01osmzz/image/upload/v1668837368/world-map_thhktd.png" alt="" />  
-      <div className="switch-outer" style={{justifyContent: turn }}>
-        <img style={{display: upArrow, marginBottom: "20px"}} className="arrow" src="https://res.cloudinary.com/di01osmzz/image/upload/v1668765842/fast-forward-double-right-arrows-symbol_3_gijmts.png" alt="" />
-        <div className={`switch-inner ${mode}`}  onClick={isSwitch}>
-          <div className={`led-${mode}`} style={{height: "4px", width: "13px", backgroundColor: dot, borderRadius: "5px"}} ></div>
-          <h3 style={{color: "white", fontSize: "10px"}}>{name}</h3>
+      <div className="switch-outer" style={{justifyContent: data.turn }}>
+        <img style={{display: data.upArrow, marginBottom: "20px"}} className="arrow" src="https://res.cloudinary.com/di01osmzz/image/upload/v1668765842/fast-forward-double-right-arrows-symbol_3_gijmts.png" alt="" />
+        <div className={`switch-inner ${data.mode}`}  onClick={isSwitch}>
+          <div className={`led-${data.mode}`} style={{height: "4px", width: "13px", backgroundColor: data.dot, borderRadius: "5px"}} ></div>
+          <h3 style={{color: "white", fontSize: "10px"}}>{data.name}</h3>
           <div className="on-icon-con" >
             <img src="https://cdn-icons-png.flaticon.com/128/4139/4139573.png" alt="" className="onIcon" />
           </div>
         </div> 
-        <img style={{display: downArrow, marginTop: "20px"}} className="arrow" src="https://res.cloudinary.com/di01osmzz/image/upload/v1668765857/fast-forward-double-right-arrows-symbol_2_kiup4n.png" alt="" />
+        <img style={{display: data.downArrow, marginTop: "20px"}} className="arrow" src="https://res.cloudinary.com/di01osmzz/image/upload/v1668765857/fast-forward-double-right-arrows-symbol_2_kiup4n.png" alt="" />
       </div>
+     
     </div>
   );
 }
